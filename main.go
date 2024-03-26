8292a2c1993bac0d9734212062c5e82ea357a833
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-26 17:16:12 +0800

    created sample guide

--- main.go
+++ main.go
@@ -13,29 +13,31 @@ func Init(api sdkplugin.PluginApi) {
 	// define the portal route
 	portalRoute := sdkhttp.VuePortalRoute{
 		RouteName: "portal.welcome",
-		RoutePath: "/welcome/:name",
+		RoutePath: "/welcome",
 		Component: "portal/Welcome.vue",
 		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
-			params := api.Http().MuxVars(r)
-			name := params["name"]
+			//params := api.Http().MuxVars(r)
+			name := "Kyle"
+			score := "97"
 
 			data := map[string]interface{}{
-				"name": name,
+				"name":  name,
+				"score": score,
 			}
 
 			api.Http().VueResponse().Json(w, data, 200)
 		},
-		Middlewares: []func(http.Handler) http.Handler{},
+		//Middlewares: []func(http.Handler) http.Handler{},
 	}
 	// register portal route
 	api.Http().VueRouter().RegisterPortalRoutes(portalRoute)
 
 	api.Http().VueRouter().PortalItemsFunc(func(r *http.Request) []sdkhttp.VuePortalItem {
 		portalItem := sdkhttp.VuePortalItem{
-			Label:       "Welcome",
-			IconPath:    "icons/welcome.png",
-			RouteName:   "portal.welcome",
-			RouteParams: map[string]string{"name": "Jhon"},
+			Label: "Welcome",
+			//IconPath:    "icons/welcome.png",
+			RouteName: "portal.welcome",
+			//RouteParams: map[string]string{"name": "Kyle"},
 		}
 		return []sdkhttp.VuePortalItem{portalItem}
 	})
--- resources/components/portal/Welcome.vue
+++ resources/components/portal/Welcome.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
+    <div class="mx-auto p-2">
         <h1>Welcome {{ flareView.data.name }}</h1>
-        <p>Some other text</p>
+
+        <ul class="list-group list-group-horizontal mb-2" style="width: 90vw; max-width: 45rem;">
+            <input type="text" class="form-control mr-2" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        </ul>
+
+        <button type="button" class="btn btn-success mb-3">Sign In</button>
+
+        <div class="rounded-lg p-4 mx-auto" style="width: 90vw; max-width: 45rem; height: 30rem; background-color: antiquewhite;">
+           <h2>High Score: {{ flareView.data.score }}</h2>
+           
+            <div class="rounded-circle " style="width: 4rem; height: 4rem; background-color: black; color: white; display: flex; justify-content: center; align-items: center;">
+                <p style="margin: 0;">bird🐤</p>
+            </div>
+        </div>
+
     </div>
 </template>
 
