3af3d56812474e6713d1285e84514ee112562219
Vince Kyle <vincekyleparcon@gmail.com>
2024-03-26 14:15:57 +0800

    created the Welcome page

---
+++ resources/components/portal/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Welcome {{ flareView.data.name }}</h1>
+        <p>Some other text</p>
+    </div>
+</template>
+
+<script>
+define(function () {
+    return {
+        props: ['flareView'],
+        template: template,
+    }
+})
+</script>
