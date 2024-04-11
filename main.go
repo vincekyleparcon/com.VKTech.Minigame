620f2c3a5f0a0b4bda01388c8ae6f292ed6ae1d9
Vince Kyle <vincekyleparcon@gmail.com>
2024-04-11 19:26:59 +0800

    remove event listener

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-
 	sdkconnmgr "github.com/flarehotspot/sdk/api/connmgr"
 	sdkhttp "github.com/flarehotspot/sdk/api/http"
 	sdkplugin "github.com/flarehotspot/sdk/api/plugin"
@@ -14,48 +11,20 @@ func main() {}
 func Init(api sdkplugin.PluginApi) {
 	// define the portal route
 	portalRoute := sdkhttp.VuePortalRoute{
-		RouteName: "portal.welcome",
-		RoutePath: "/welcome",
-		Component: "portal/Welcome.vue",
-		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
-			//params := api.Http().MuxVars(r)
-			name := "Kyle"
-			score := "97"
-
-			data := map[string]interface{}{
-				"name":  name,
-				"score": score,
-			}
-
-			api.Http().VueResponse().Json(w, data, 200)
-		},
-		//Middlewares: []func(http.Handler) http.Handler{},
+		RouteName: "portal.game",
+		RoutePath: "/game",
+		Component: "portal/Game.vue",
 	}
+
 	// register portal route
 	api.Http().VueRouter().RegisterPortalRoutes(portalRoute)
 
 	api.Http().VueRouter().PortalItemsFunc(func(clnt sdkconnmgr.ClientDevice) []sdkhttp.VuePortalItem {
 		portalItem := sdkhttp.VuePortalItem{
-			Label: "Welcome",
-			//IconPath:    "icons/welcome.png",
-			RouteName: "portal.welcome",
-			//RouteParams: map[string]string{"name": "Kyle"},
+			Label:     "Play Game",
+			IconPath:  "icons/UFO.png",
+			RouteName: "portal.game",
 		}
 		return []sdkhttp.VuePortalItem{portalItem}
 	})
-
-	api.Http().HttpRouter().PluginRouter().Post("/payments/received", func(w http.ResponseWriter, r *http.Request) {
-		var data struct {
-			Amount int `json:"amount"`
-		}
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		score := map[string]int{"score": data.Amount}
-		//fmt.Fprintf(w, "Payment Received: %d", data.Amount)
-		api.Http().VueResponse().Json(w, score, 200)
-
-	}).Name("payment.received")
 }
---
+++ resources/components/portal/Game.vue
@@ -0,0 +1,51 @@
+<template>
+  
+    <canvas id="canvas1"></canvas>
+
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #canvas1 {
+    border: 5px solid black;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: blue;
+    max-width: 100%;
+    max-height: 100%;
+  }
+</style>
+
+<script>
+  define(function(){
+    var component = {
+      template: template,
+      mounted: function() {
+
+        function Game(canvas, context){
+          this.canvas = canvas;
+          this.context = context;
+          this.width = this.canvas.width;
+          this.height = this.canvas.height;
+        }
+
+
+          const canvas = document.getElementById('canvas1');
+          const ctx = canvas.getContext('2d');
+          canvas.width = 720;
+          canvas.height = 720;
+     
+
+        var game = new Game(canvas, ctx);
+      }
+    };
+
+    return component;
+  })
+</script>
