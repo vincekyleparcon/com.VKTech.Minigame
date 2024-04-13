a232c3d8c5c1733e1884caacd4e9a5b816369071
Vince Kyle <vincekyleparcon@gmail.com>
2024-04-13 17:03:42 +0800

    Methods transfered

--- resources/components/portal/Game.vue
+++ resources/components/portal/Game.vue
@@ -30,9 +30,75 @@
 
         function Game(canvas, context){
           this.canvas = canvas;
-          this.context = context;
+          this.ctx = context;
           this.width = this.canvas.width;
           this.height = this.canvas.height;
+          this.baseHeight = 720;
+          this.ratio = this.height / this.baseHeight;
+          this.background = new Background(this);
+          this.player = new Player(this);
+          this.obstacles = [];
+          this.numberOfObstacles = 200;
+          this.gravity;
+          this.speed;
+
+
+          this.resize(window.innerWidth, window.innerHeight);
+
+          window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+          });
+          //mouse control
+          this.canvas.addEventListener('mousedown', e => {
+            this.player.flap();
+          });
+          //keyboard control
+          window.addEventListener('keydown', e => {
+            console.log(e.key);
+            if (e.key === 'ArrowUp' || e.key === ' ' || e.key === 'w') {
+              this.player.flap();
+            };
+          })
+          //touchscreen
+          this.canvas.addEventListener('touchstart', e => {
+            this.player.flap();
+          })
+        }
+        Game.prototype.resize = function(width, height){
+          this.canvas.width = width;
+          this.canvas.height = height;
+          this.ctx.fillStyle = 'red';
+          this.width = this.canvas.width;
+          this.height = this.canvas.height;
+          this.ratio = this.height / this.baseHeight;
+
+          this.gravity = 0.17 * this.ratio;
+          this.speed = 3 * this.ratio;
+          this.background.resize();
+          this.player.resize();
+          this.createObstacles();
+          this.obstacles.forEach(obstacle => {
+            obstacle.resize();
+          });
+        }
+        Game.prototype.render = function(){
+          //this.ctx.fillStyle = 'red';
+          this.background.update();
+          this.background.draw();
+          this.player.update();
+          this.player.draw();
+          this.obstacles.forEach(obstacle => {
+            obstacle.update();
+            obstacle.draw();
+          });
+        }
+        Game.prototype.createObstacles = function(){
+          this.obstacles = [];
+        const firstX = this.baseHeight * this.ratio;
+        const obstacleSpacing = 800 * this.ratio;
+        for (let i = 0; i < this.numberOfObstacles; i++){
+            this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
+        }
         }
 
         function Player(game){
@@ -78,7 +144,6 @@
           };
         }
         
-
         function Obstacle(game, x) {
           this.game = game;
           this.spriteWidth = 110;
@@ -145,8 +210,15 @@
           const ctx = canvas.getContext('2d');
           canvas.width = 720;
           canvas.height = 720;
-     
-        var game = new Game(canvas, ctx);
+          const game = new Game(canvas, ctx);
+          game.render();
+
+        function animate() {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          game.render();
+          requestAnimationFrame(animate);
+        }
+        requestAnimationFrame(animate);
       }
     };
 
