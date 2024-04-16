83f336a90e30ef09556175eabc3982756bba353d
Vince Kyle <vincekyleparcon@gmail.com>
2024-04-16 14:57:48 +0800

    Obsctacle Class and methods added

--- resources/components/portal/Game.vue
+++ resources/components/portal/Game.vue
@@ -151,6 +151,31 @@
           this.x2 = 0;
         }
 
+        function Obstacle(game, x){
+          this.game = game;
+          this.spriteWidth = 110;
+          this.spriteHeight = 110;
+          this.scaledWidth = this.spriteWidth * this.game.ratio;
+          this.scaledHeight = this.spriteHeight * this.game.ratio;
+          this.x = x;
+          this.y = Math.random() * this.game.height - this.scaledHeight;
+          this.speedY = Math.random() < 0.5 ? -1 : 1;
+        }
+        Obstacle.prototype.update = function(){
+          this.x -= this.game.speed * 1.5;
+          this.y += this.speedY;
+          if (this.y <= this.scaledHeight * -0.2 || this.y >= this.game.height - this.scaledHeight) {
+            this.speedY *= -1;
+          }
+        }
+        Obstacle.prototype.draw = function () {
+          this.game.ctx.fillRect(this.x, this.y, this.scaledWidth, this.scaledHeight);
+        }
+        Obstacle.prototype.resize = function () {
+          this.scaledWidth = this.spriteWidth * this.game.ratio;
+          this.scaledHeight = this.spriteHeight * this.game.ratio;
+        }
+
 
 
             const canvas = document.getElementById('canvas1');
