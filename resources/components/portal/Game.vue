2af4ab0d462e19a9078948de262aa12f32c1d531
Vince Kyle <vincekyleparcon@gmail.com>
2024-04-16 14:54:40 +0800

    Background cleanup

--- resources/components/portal/Game.vue
+++ resources/components/portal/Game.vue
@@ -2,8 +2,8 @@
   <div>
     <canvas id="canvas1"></canvas>
     <div class="assets">
-      <img id="background" src="">
-      <img id="background2" src="">
+      <img id="background" src='<% .Helpers.AssetPath "backgrounds/background trees.png" %>'>
+      <img id="background2" src='<% .Helpers.AssetPath "backgrounds/background sky.png" %>'>
     </div>
   </div>
 </template>
@@ -16,11 +16,9 @@
   }
 
   #canvas1 {
-    border: 5px solid black;
     position: absolute;
     top: 0;
     left: 0;
-    background-color: blue;
     max-width: 100%;
     max-height: 100%;
   }
@@ -78,13 +76,14 @@
           this.game = game;
           this.x = 20;
           this.y;
+          this.spriteWidth = 200;
+          this.spriteHeight = 200;
           this.width = 200;
           this.height = 200;
-          this.speedY;      
+          this.speedY;
           this.flapSpeed;
         }
         Player.prototype.draw = function() {
-          this.game.ctx.fillStyle = 'red';
           this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
         }
         Player.prototype.update = function() {
@@ -98,8 +97,8 @@
           }
         }
         Player.prototype.resize = function() {
-          this.width = this.width * this.game.ratio;
-          this.height = this.height * this.game.ratio;
+          this.width = this.spriteWidth * this.game.ratio;
+          this.height = this.spriteHeight * this.game.ratio;
           this.y = this.game.height * 0.5 - this.height * 0.5;
           this.speedY = -4 * this.game.ratio;
           this.flapSpeed = 7 * this.game.ratio;
@@ -119,24 +118,26 @@
         function Background(game) {
           this.game = game;
           this.image = document.getElementById('background');
-          // this.image2 = document.getElementById('background2');
+          this.image2 = document.getElementById('background2');
           this.width = 2400;
           this.height = this.game.baseHeight;
           this.scaledWidth;
           this.scaledHeight;
           this.x;
-          // this.x2;
+          this.x2;
         }
         Background.prototype.update = function(){
           this.x -= this.game.speed * 1.2;
           if (this.x <= -this.scaledWidth) this.x = 0;
+          this.x2 -= this.game.speed * .6;
+          if (this.x2 <= -this.scaledWidth) this.x2 = 0;
         }
         Background.prototype.draw = function () {
-          // this.game.ctx.drawImage(this.image2, this.x2, 0, this.scaledWidth, this.scaledHeight);
-          // this.game.ctx.drawImage(this.image2, this.x2 + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
-          // if (this.game.canvas.width >= this.scaledWidth) {
-          //   this.game.ctx.drawImage(this.image2, this.x2 + this.scaledWidth * 2 - 2, 0, this.scaledWidth, this.scaledHeight);
-          // }
+          this.game.ctx.drawImage(this.image2, this.x2, 0, this.scaledWidth, this.scaledHeight);
+          this.game.ctx.drawImage(this.image2, this.x2 + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
+          if (this.game.canvas.width >= this.scaledWidth) {
+            this.game.ctx.drawImage(this.image2, this.x2 + this.scaledWidth * 2 - 2, 0, this.scaledWidth, this.scaledHeight);
+          }
           this.game.ctx.drawImage(this.image, this.x, 0, this.scaledWidth, this.scaledHeight);
           this.game.ctx.drawImage(this.image, this.x + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
           if (this.game.canvas.width >= this.scaledWidth) {
@@ -147,8 +148,10 @@
           this.scaledWidth = this.width * this.game.ratio;
           this.scaledHeight = this.height * this.game.ratio;
           this.x = 0;
-          // this.x2 = 0;
+          this.x2 = 0;
         }
+
+
 
             const canvas = document.getElementById('canvas1');
             const ctx = canvas.getContext('2d');
